462effb85f7c5da73d49803d7be39cc774fa41a0
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-03-07 12:59:24 -0800

    Added some button styling to login

--- autodidact/src/homepage.js
+++ autodidact/src/homepage.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 
 class Homepage extends Component {
     render() {
         return (
-            <div className='container-fludi' id='homepage'>
+            <div className='container-fluid' id='homepage'>
                 <div id='homepage-div-block'>
                     
                         <h2>Hello, Autodidact!</h2>
                             <p> An app to help self-learners organize their self-learning. </p>
-                            <button className='content-button'>Build Your Curriculum</button>
+                            <button className='content-button'><Link to='/login' id='homepage-login-link'>Build Your Curriculum</Link></button>
                             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
                     
                 </div>
--- autodidact/src/login-page.js
+++ autodidact/src/login-page.js
@@ -4,15 +4,15 @@ class LoginPage extends Component {
     render() {
         return (
             <div className='page'>    
-                <h2>Welcome! Login or create an account to get started with Autodidact.</h2><br/>
+                <h3>Welcome! Login or create an account to get started with Autodidact.</h3><br/>
 
                 <form action='/handle-login' method='POST'>
                     
                     Email: <input type='text' name='user_email'/><br/><br/>
                     Password: <input type='password' name='user_pw' maxLength='70'/><br/><br/>
                     
-                    <input type='submit' name='login' value='Login'/> &nbsp;
-                    <input type='submit' name='create-account' value='Create New Account'/>
+                    <input type='submit' name='login' value='Login' className='content-button'/> &nbsp;
+                    <input type='submit' name='create-account' value='Create New Account' className='content-button'/>
                 
                 </form>
             </div>
